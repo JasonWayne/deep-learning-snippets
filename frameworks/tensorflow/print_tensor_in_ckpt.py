7c64e065752fcbb902494d757a41140f42facf05
Wenjie Wu <wuwenjie0102@gmail.com>
2019-11-08 08:52:57 +0800

    Update print_tensor_in_ckpt.py

--- frameworks/tensorflow/print_tensor_in_ckpt.py
+++ frameworks/tensorflow/print_tensor_in_ckpt.py
@@ -1,4 +1,8 @@
-# common usage: python print_tensor_in_ckpt.py > tensors.txt
+'''
+common usage: 
+  1. put this script in ckpt folder
+  2. python print_tensor_in_ckpt.py > tensors.txt
+'''
 # ref: https://stackoverflow.com/questions/38218174/how-do-i-find-the-variable-names-and-values-that-are-saved-in-a-checkpoint
 import tensorflow as tf
 from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
